0fc33987c51ade633aa7c2a8310737fba08172c9
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-11-08 23:00:53 +0900

    いんでんとがきになった。

--- root/models/Model.js
+++ root/models/Model.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose'),
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-    Schema = mongoose.Schema,
-    {%= main_model %} = new Schema({
-        id: {
-            type: Number,
-            index: { unique: true }
-        }
-    });
+var {%= main_model %} = new Schema({
+    id: {
+        type: Number,
+        index: { unique: true }
+    }
+});
 
 module.exports = mongoose.model('{%= main_model %}', {%= main_model %});
