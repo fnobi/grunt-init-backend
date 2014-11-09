06fc9e56635ef7fbad920acec615f64ca84ca95b
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-11-09 21:30:21 +0900

    errorハンドリングのみなおし

--- root/routes/error.js
+++ root/routes/error.js
@@ -1,9 +1,11 @@
 module.exports = function (err, req, res) {
     var format = req.param('format');
 
+    res.status(res.statusCode || 500);
+
     var json = {
         statusCode: res.statusCode,
-        error: err ? err.toString() : null
+        error: err || null
     };
 
     if (format == 'json') {
