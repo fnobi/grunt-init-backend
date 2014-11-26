03aa446a077b1ca03e9315110e2122c11987822a
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-11-27 02:15:09 +0900

    soundcloud authのリファクタ

--- root/lib/auth-soundcloud.js
+++ root/lib/auth-soundcloud.js
@@ -1,12 +1,16 @@
 var config = require('config');
 var SoundCloudAPI = require('soundcloud-node');
 
+var CLIENT_ID = config.soundcloud.client_id;
+var CLIENT_SECRET = config.soundcloud.client_secret;
+var REDIRECT_URI = config.locals.url + 'soundcloud/callback';
+
 module.exports = {
     getClient: function () {
         return new SoundCloudAPI(
-            config.soundcloud.client_id,
-            config.soundcloud.client_secret,
-            config.soundcloud.redirect_uri
+            CLIENT_ID,
+            CLIENT_SECRET,
+            REDIRECT_URI
         );
     },
     getClientWithToken: function (token) {
@@ -14,10 +18,35 @@ module.exports = {
         client.setToken(token);
         return client;
     },
-    getAccessToken: function (req, res) {
-        return req.session.soundcloud_access_token;
+    getUserToken: function (session) {
+        return session.soundcloud_access_token;
     },
-    setAccessToken: function (req, res, token) {
-        req.session.soundcloud_access_token = token;
+    setUserToken: function (session, token) {
+        session.soundcloud_access_token = token;
+    },
+    deleteUserToken: function (session) {
+        delete session.soundcloud_access_token;
+    },
+    getAuthURI: function () {
+        var client = this.getClient();
+        return client.getConnectUrl();
+    },
+    requestAccessToken: function (req, callback) {
+        var code = req.param('code');
+        var client = this.getClient();
+
+        if (!code) {
+            callback('invalid code.');
+            return;
+        }
+
+        client.getToken(code, function (err, tokens) {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            callback(null, tokens.access_token);
+        });
     }
 };
--- root/routes/soundcloud.js
+++ root/routes/soundcloud.js
@@ -6,34 +6,36 @@ var soundcloud = require(__dirname + '/../lib/auth-soundcloud');
 
 module.exports = {
     login: function (req, res) {
-        var client = soundcloud.getClient();
-        res.redirect(301, client.getConnectUrl());
+        res.redirect(soundcloud.getAuthURI());
     },
     loginCallback: function (req, res) {
-        var code = req.param('code');
-        var client = soundcloud.getClient();
-
-        client.getToken(code, function (err, tokens) {
+        soundcloud.requestAccessToken(req, function (err, accessToken) {
             if (err) {
-                res.status(400);
-                return handleError(err, req, res);
+                res.status(500);
+                handleError(err.data, req, res);
+                return;
             }
-
-            soundcloud.setAccessToken(req, res, tokens.access_token);
+            soundcloud.setUserToken(req.session, accessToken);
             res.redirect('/');
         });
+    },
+    logout: function (req, res) {
+        soundcloud.deleteUserToken(req.session);
+        res.redirect('/');
     },
     stream: function (req, res) {
         var trackId = req.param('track_id');
         if (!trackId) {
             res.status(400);
-            return handleError('track id is not specified.', req, res);
+            handleError('track id is not specified.', req, res);
+            return;
         }
 
-        var accessToken = req.param('token') || soundcloud.getAccessToken(req, res);
+        var accessToken = req.param('token') || soundcloud.getUserToken(req.session);
         if (!accessToken) {
             res.status(400);
-            return handleError('not authorized', req, res);
+            handleError('not authorized', req, res);
+            return;
         }
 
         var client = soundcloud.getClientWithToken(accessToken);
@@ -41,7 +43,8 @@ module.exports = {
 
         client.get('/' + apiPath, function (err, stream) {
             if (err) {
-                return handleError(err, req, res);
+                handleError(err, req, res);
+                return;
             }
             res.redirect(302, stream.location);
         });
